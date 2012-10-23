f92f53094e642ee86b8183dec48a612d19d809e0
Aaron Evans <aaron@fedora.lenovo.foxfish.lcl>
2012-10-23 07:47:42 -0600

    Building site module that will return page objects with shared browser.  Currently only sets environment base url.

--- runwithjenkins/features/load_home_page.feature
+++ runwithjenkins/features/load_home_page.feature
@@ -1,10 +1,26 @@
+@smoketest
 Feature: Website availability
 	In order to verify the Dollar Shave Club website is up
 	As a user
 	I want to check that the home page loads
 
-Scenario: visit home page
-	Given I have an open browser 
-	When I go to the url "www.dollarshaveclub.com"
-	Then I should see the title "Men's Razors, Buy Razor Blades Online | Dollar Shave Club"
+	@homepage
+	Scenario: load home page
+		Given I have an open browser 
+		When I go to the url "www.dollarshaveclub.com"
+		Then I should see the title "Men's Razors, Buy Razor Blades Online | Dollar Shave Club"
 
+
+	@homepage @content @development
+	Scenario Outline: verify home page content
+		Given I am on the dollarshaveclub.com home page
+		When I check the page for <element>
+		Then I should see the <content>
+
+		Examples:
+		| element       | content                                                                     |
+		| logo          | background: url("../images/logo.png")                                       |
+		| video         | iframe http://www.youtube.com/embed/ZUG9qYTJMsI?rel=0&autohide=1&autoplay=0 |
+		| login_link    | Login https://www.dollarshaveclub.com/customer/account/login                |
+		| register_link | Register http://www.dollarshaveclub.com/select-blade                        |
+		| copyright     | "©2012 Dollar Shave Club All Rights Reserved"                               |
---
+++ runwithjenkins/features/step_definitions/verify_home_page_content.rb
@@ -0,0 +1,53 @@
+#verify_home_page_content.rb
+
+class DollarShaveClub
+
+	def initialize(browser, environment="production")
+	end
+end
+
+Given /^I am on the dollarshaveclub\.com home page$/ do
+  @browser = Watir::Browser.new
+  @site = DollarShaveClub.new(@browser)
+  @browser.goto url
+end
+
+When /^I check the page for logo$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the background: url\("(.*?)"\)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the page for video$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the iframe http:\/\/www\.youtube\.com\/embed\/ZUG(\d+)qYTJMsI\?rel=(\d+)&autohide=(\d+)&autoplay=(\d+)$/ do |arg1, arg2, arg3, arg4|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the page for login_link$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the Login https:\/\/www\.dollarshaveclub\.com\/customer\/account\/login$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the page for register_link$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the Register http:\/\/www\.dollarshaveclub\.com\/select\-blade$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the page for copyright$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
